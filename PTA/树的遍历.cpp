0065d25ce576deedb403d6f487cafeb5f982dda1
Qi <56908202+qi-xmu@users.noreply.github.com>
2020-10-21 14:55:09 +0800

    Update 树的遍历.cpp

--- PTA/树的遍历.cpp
+++ PTA/树的遍历.cpp
@@ -11,7 +11,6 @@ BiTree rebuild(int post[], int in[], int len, BiTree& T)
 	// 长度为0时，递归退出
 	if (len == 0)
 		return NULL;
-
 	// 根结点
 	T = (BiTree)malloc(sizeof(BiNode));
 	int root = post[len - 1];
@@ -19,15 +18,17 @@ BiTree rebuild(int post[], int in[], int len, BiTree& T)
 	// 寻找root在in的位置
 	int p = 0;
 	for (; p < len; p++)
-	{
 		if (in[p] == root)
 			break;
-	}
-
-	// 
+	// 递归
 	T->lc = rebuild(post, in, p, T->lc);
 	T->rc = rebuild(&post[p], &in[p + 1], len - p - 1, T->rc);
 	return T;
+}
+
+void output(BiTree T)
+{
+
 }
 
 int main()
@@ -37,19 +38,13 @@ int main()
 	int inorder[50];	// 中序排列
 	// 输入
 	scanf("%d", &n);
-
-	//BiTree T;
-
 	for (int i = 0; i < n; i++)
-	{
 		scanf("%d", &postorder[i]);
-	}
 	for (int i = 0; i < n; i++)
-	{
 		scanf("%d", &inorder[i]);
-	}
+
 	BiTree T = NULL;
 	rebuild(postorder, inorder, 7, T);
-
+	output(T);
 	return 0;
 }
