ea3876154906914a9ca2797252dce8401cda90b0
Qi <56908202+qi-xmu@users.noreply.github.com>
2020-10-21 15:42:56 +0800

    Update 树的遍历.cpp

--- PTA/树的遍历.cpp
+++ PTA/树的遍历.cpp
@@ -5,6 +5,8 @@ typedef struct BiNode
 	int data;
 	struct BiNode* lc, * rc;
 }BiNode, * BiTree;
+
+
 // 重构二叉树
 BiTree rebuild(int post[], int in[], int len, BiTree& T)
 {
@@ -28,7 +30,32 @@ BiTree rebuild(int post[], int in[], int len, BiTree& T)
 
 void output(BiTree T)
 {
+	BiTree queue[101];
+	int base = 0;
+	int top = 0;
+	queue[top++] = T;
+	int res[50];
+	int p = 0;
+	while (1)
+	{
+		if (top == base)
+			break;
+		BiTree Root = queue[base++];
+		res[p++]=Root->data;
+		if (Root->lc) {
+			queue[top++] = Root->lc;
+		}
+		if (Root->rc){
+			queue[top++] = Root->rc;
+		}
+	}
 
+	for (int i = 0; i < p; i++)
+	{
+		printf("%d", res[i]);
+		if (i != p - 1)
+			printf(" ");
+	}
 }
 
 int main()
@@ -42,9 +69,8 @@ int main()
 		scanf("%d", &postorder[i]);
 	for (int i = 0; i < n; i++)
 		scanf("%d", &inorder[i]);
-
 	BiTree T = NULL;
-	rebuild(postorder, inorder, 7, T);
+	rebuild(postorder, inorder, n, T);
 	output(T);
 	return 0;
 }
