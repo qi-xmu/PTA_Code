22d53270c737b9fc855245b7dcf26a96974aaec0
Qi <56908202+qi-xmu@users.noreply.github.com>
2020-10-21 14:08:56 +0800

    Update 树的遍历.cpp

--- PTA/树的遍历.cpp
+++ PTA/树的遍历.cpp
@@ -4,13 +4,25 @@ typedef struct BiNode
 {
 	int data;
 	struct BiNode* lc, * rc;
-}Bitree;
+}BiNode, *BiTree;
 
-int rebuild(int post[],int in[],int len)
+int rebuild(int post[],int in[],int len, BiTree T)
 {
+
 	// 根结点
 	int root = post[len];
-
+	T->data = root;
+	// 寻找root在in的位置
+	int p = 0;
+	for (; p < len; p++)
+	{
+		if (in[p] == root)
+			break;
+	}
+	T->lc = (BiTree)(sizeof(BiNode));
+	rebuild(post, in, p, T->lc);
+	T->rc = (BiTree)(sizeof(BiNode));
+	rebuild(&post[len], &in[len + 1], len - p,T->rc);
 
 	return root;
 }
@@ -22,11 +34,14 @@ int main()
 	int inorder[50];	// 中序排列
 	// 输入
 	scanf("%d", &n);
-	for (int i = 1; i <= n; i++)
+
+	//BiTree T;
+
+	for (int i = 0; i < n; i++)
 	{
 		scanf("%d", &postorder[i]);
 	}
-	for (int i = 1; i <= n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		scanf("%d", &inorder[i]);
 	}
